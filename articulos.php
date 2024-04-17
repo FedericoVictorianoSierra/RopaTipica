35132b426017c0908794ff90d84622eae3224c2d
Minishxco <71857813+FedericoVictorianoSierra@users.noreply.github.com>
2024-04-17 00:52:40 -0600

    carrito

--- articulos.php
+++ articulos.php
@@ -32,6 +32,8 @@ if (isset($_POST['agregar_carrito'])) {
         mysqli_query($conexion, $sql_insertar);
     }
 
+    echo "<script>alert('Artículo agregado al carrito correctamente.');</script>";
+
     // Redirigir al usuario a la página del carrito de compras
     //echo "<script>location.href='carrito.php';</script>";
 }
@@ -188,4 +190,27 @@ if (isset($_POST['agregar_carrito'])) {
             block: 'start' // define la posición del elemento de referencia (el inicio del elemento enfocado) en relación con la ventana de visualización
         });
     }
+</script>
+
+<script>
+    // Función para guardar la posición del scroll
+    function guardarPosicionScroll() {
+        localStorage.setItem('posicionScroll', window.pageYOffset);
+    }
+
+    // Función para restaurar la posición del scroll
+    function restaurarPosicionScroll() {
+        var posicionScroll = localStorage.getItem('posicionScroll');
+        if (posicionScroll) {
+            window.scrollTo(0, posicionScroll);
+            localStorage.removeItem('posicionScroll'); // Elimina la posición guardada para que no se restaure cada vez
+        }
+    }
+
+    // Guardar la posición del scroll cuando la página se esté cerrando o al realizar un evento de scroll
+    window.addEventListener('beforeunload', guardarPosicionScroll);
+    window.addEventListener('scroll', guardarPosicionScroll);
+
+    // Restaurar la posición del scroll cuando la página se carga
+    window.addEventListener('load', restaurarPosicionScroll);
 </script>
